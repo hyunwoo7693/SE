80acee70cd5fd5d8ff6102612902307bb5dae903
hyunwoo7693 <guswkd7693@gmail.com>
2026-05-10 21:58:17 +0900

    usecase_diagram 코드 작성

---
+++ usecase_diagrams/Admin.cpp
@@ -0,0 +1,70 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "Admin.h"
+
+/**
+ * Admin implementation
+ */
+
+
+void Admin::createSurvey() {
+
+}
+
+/**
+ * @param surveyId
+ * @return bool
+ */
+bool Admin::deleteSurvey(int surveyId) {
+    return false;
+}
+
+/**
+ * @param surveyId
+ * @return void
+ */
+void Admin::viewStatistics(int surveyId) {
+    return;
+}
+
+/**
+ * @param id
+ * @param pw
+ * @return bool
+ */
+bool Admin::login(string id, string pw) {
+    return false;
+}
+
+/**
+ * @return void
+ */
+void Admin::logout() {
+    return;
+}
+
+/**
+ * @param title
+ * @return void
+ */
+void Admin::registerSurvey(string title) {
+    return;
+}
+
+/**
+ * @return void
+ */
+void Admin::listSurveys() {
+    return;
+}
+
+/**
+ * @param surveyId
+ * @return void
+ */
+void Admin::viewDetails(int surveyId) {
+    return;
+}
---
+++ usecase_diagrams/Survey.h
@@ -0,0 +1,18 @@
+/**
+ * Project Untitled
+ */
+
+
+#ifndef _SURVEY_H
+#define _SURVEY_H
+
+class Survey {
+public: 
+    void Attribute1;
+private: 
+    int surveyId;
+    string title;
+    string content;
+};
+
+#endif //_SURVEY_H
